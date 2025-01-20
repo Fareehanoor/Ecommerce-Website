f3c30e0736b241f68ad330c5c3bee013124c642f
Fareeha Noor <fareehaabbas21@gmail.com>
2025-01-20 22:27:37 +0500

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Products from "./components/Products";
+import ErrorPage from "./components/ErrorPage";
+
 function App() {
   return (
     <>
-      <div>Start Ecommerece Website </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
