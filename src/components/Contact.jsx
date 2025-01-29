6211b6c5a852f132bd292d442ca98fd7990048ce
Fareeha Noor <fareehaabbas21@gmail.com>
2025-01-29 23:36:16 +0500

    working on navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,9 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div>Contact</div>
+    </>
+  );
 };
 
 export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
+// import styled from "styled-components";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div>home</div>;
 };
+
+// const Wrapper = styled.section`
+//   /* background-color: ${({ theme }) => theme.colors.bg}; */
+//   /* background-color: aquamarine; */
+//   /* width: 20rem; */
+//   /* height: 20rem; */
+// `;
 
 export default Home;
