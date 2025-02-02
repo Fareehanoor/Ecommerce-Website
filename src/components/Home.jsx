9799c31374ede3d7235befeea6485009cf6b1ac5
Fareeha Noor <fareehaabbas21@gmail.com>
2025-02-02 21:23:47 +0500

    add services section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import HeroSection from "../views/HeroSection";
+import Services from "./Services";
+import Trusted from "./Trusted";
 
 const Home = () => {
   const data = {
     name: "thapa store",
   };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
