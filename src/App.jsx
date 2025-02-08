3fe81f7eee748ed6b75ed8a129238dac0cc9515a
Fareeha Noor <fareehaabbas21@gmail.com>
2025-02-08 23:07:11 +0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import Header from "./views/Header";
 import Cart from "./components/Cart";
+import Footer from "./views/Footer";
 
 function App() {
   const theme = {
     colors: {
       heading: "rgb(24 24 29)",
-      text: "rgba(29,29,29,0.8)",
+      text: "rgba(29 ,29, 29, .8)",
       white: "#fff",
-      black: "#212529",
+      black: " #212529",
       helper: "#8490ff",
+
       bg: "#F6F8FA",
-      footer_bg: "0a1435",
+      footer_bg: "#0a1435",
       btn: "rgb(98 84 243)",
       border: "rgba(98, 84, 243, 0.5)",
       hr: "#ffffff",
@@ -47,6 +49,7 @@ function App() {
             <Route path="cart" element={<Cart />} />
             <Route path="*" element={<ErrorPage />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </ThemeProvider>
     </>
