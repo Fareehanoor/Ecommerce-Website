97ca8344386c325cf93c5ae8debcbf9e60274186
Fareeha Noor <fareehaabbas21@gmail.com>
2025-03-26 13:28:38 +0500

    working on cart page

--- src/reducers/cartReducer.jsx
+++ src/reducers/cartReducer.jsx
@@ -1,7 +1,22 @@
 const cartReducer = (state, action) => {
   if (action.type === "add_to_cart") {
     let { id, color, amount, product } = action.payload;
-    console.log("cartReducer::::::::::", id, color, amount, product);
+    console.log("cartReducer::", id, color, amount, product);
+
+    let cartProduct;
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      amount,
+      color,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
   }
   return state;
 };
